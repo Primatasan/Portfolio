ce88fb707bff2e698d9634f7f097b2e36200406a
Carlos Sanmartin <contato@carlosmsdesign.com.br>
2023-07-02 00:10:29 -0300

    Portfolio page implementation

--- src/components/Portfolio/PortfolioCard/index.jsx
+++ src/components/Portfolio/PortfolioCard/index.jsx
@@ -3,7 +3,7 @@ import './PortfolioCard.css'
 export default function PortfolioCard(){
     return (
         <div>
-            <h1>Teste</h1>
+            
         </div>
     )
 }
--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,10 +1,13 @@
 import './Portfolio.css'
-import PortfolioCard from './PortfolioCard'
+// import PortfolioCard from './PortfolioCard'
+import ReactMarkdown from 'react-markdown'
+import { projetos } from './projetos.json'
 
 export default function Portfolio(){
+
     return(
-        <div>
-            <PortfolioCard />
-        </div>
+        <ReactMarkdown>
+            {projetos[0].blocoA}
+        </ReactMarkdown>
     )
 }
