96854f66335ccb63878046c7bc409c8de6971e3a
Carlos Sanmartin <contato@carlosmsdesign.com.br>
2023-07-03 15:47:49 -0300

    Portfolio page and cards implementation

--- src/components/Portfolio/PortfolioCard/index.jsx
+++ src/components/Portfolio/PortfolioCard/index.jsx
@@ -1,9 +1,15 @@
 import './PortfolioCard.css'
+import { Link } from "react-router-dom";
 
-export default function PortfolioCard(){
+export default function PortfolioCard({image, title, subtitle}){
     return (
-        <div>
-            
+        <div className='portfolio_card'>
+            <img src={image} alt={title} />
+            <div className='portfolio_card_infomation'>
+                <h3>{title}</h3>
+                <h4>{subtitle}</h4>
+                <Link className='botao'>Veja mais</Link>
+            </div>
         </div>
     )
 }
--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,13 +1,22 @@
 import './Portfolio.css'
-// import PortfolioCard from './PortfolioCard'
-import ReactMarkdown from 'react-markdown'
-import { projetos } from './projetos.json'
+import PortfolioCard from './PortfolioCard'
+
+import {projetos} from './projetos.json'
 
 export default function Portfolio(){
 
     return(
-        <ReactMarkdown>
-            {projetos[0].blocoA}
-        </ReactMarkdown>
+        <div className='portfolio_cards_box'>
+            <div className='wrapper'>
+                {projetos.map((e) => (
+                    <PortfolioCard 
+                        key={e.id} 
+                        image={`src/assets/portfolio/${e.id}/${e.id}.png`} 
+                        title={e.titulo} 
+                        subtitle={e.subtitulo} 
+                    />
+                ))}
+            </div>
+        </div>
     )
 }
